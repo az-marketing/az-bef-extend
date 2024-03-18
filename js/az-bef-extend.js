737f23a6c324838e289115ea5212658bcdf74928
krystianflores <krystianflores@arizona.edu>
2024-03-18 17:00:10 +0000

    update to select tab

--- js/az-bef-extend.js
+++ js/az-bef-extend.js
@@ -1,23 +1,17 @@
-((Drupal) => {
-  Drupal.behaviors.nestedUl = {
-    attach: (context, settings) => {
-      const listItems = context.querySelectorAll("li");
-      listItems.forEach((li) => {
-        if (li.querySelector("ul")) {
-          li.classList.add("has-nested-ul");
-        }
-        const nest = context.querySelectorAll(".has-nested-ul div");
-        nest.forEach((lab) => {
-          lab.addEventListener("click", function (e) {
-            let tog = e.target.parentElement;
-            if (tog.classList.contains("open")) {
-              tog.classList.remove("open");
-            } else {
-              tog.classList.add("open");
-            }
-          });
-        });
-      });
-    },
-  };
-})(Drupal);
+const listItems = document.querySelectorAll("li");
+listItems.forEach((li) => {
+  if (li.querySelector("ul")) {
+    li.classList.add("has-nested-ul");
+  }
+  const nest = document.querySelectorAll(".has-nested-ul div");
+  nest.forEach((lab) => {
+    lab.addEventListener("click", function (e) {
+      let tog = e.target.parentElement;
+      if (tog.classList.contains("open")) {
+        tog.classList.remove("open");
+      } else {
+        tog.classList.add("open");
+      }
+    });
+  });
+});
