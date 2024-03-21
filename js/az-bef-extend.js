b0b16c5b92354dfefe9f931f21e2fe24668d582f
krystianflores <krystianflores@arizona.edu>
2024-03-21 21:40:06 +0000

    update to scss, and js

--- js/az-bef-extend.js
+++ js/az-bef-extend.js
@@ -2,16 +2,9 @@ const listItems = document.querySelectorAll("li");
 listItems.forEach((li) => {
   if (li.querySelector("ul")) {
     li.classList.add("has-nested-ul");
-  }
-  const nest = document.querySelectorAll(".has-nested-ul div");
-  nest.forEach((lab) => {
-    lab.addEventListener("click", function (e) {
-      let tog = e.target.parentElement;
-      if (tog.classList.contains("open")) {
-        tog.classList.remove("open");
-      } else {
-        tog.classList.add("open");
-      }
+    li.addEventListener("click", function (e) {
+      const tog = e.target;
+      tog.classList.toggle("open");
     });
-  });
+  }
 });
